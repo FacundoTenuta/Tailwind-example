54a42098592f2acc2335cdcd4d2c7c7d54eac2cc
Facundo Tenuta <facu_201117@hotmail.com>
2021-02-12 15:43:27 -0300

    tailwind component

---
+++ src/TailwindExample.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const TailwindExample = () => {
+    return (
+        <div className="container bg-gray-800 h-screen">
+
+        </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { TailwindExample } from './TailwindExample';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <TailwindExample />,
   document.getElementById('root')
 );
